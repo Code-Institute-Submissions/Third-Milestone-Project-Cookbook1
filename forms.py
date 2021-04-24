de1c2afd90b875b427b410db1d3678fad61c3d50
ZitaBalint <zita.balint1072@gmail.com>
2021-04-24 21:40:24 +0000

    Imported fields to file

---
+++ forms.py
@@ -0,0 +1,4 @@
+from wtforms import FlaskForm
+from wtforms import  StringField, TexAreatField, BooleanField, PasswordField, SubmitField, validators
+from wtforms.validators import Length, EqualTo, DataRequired
+
