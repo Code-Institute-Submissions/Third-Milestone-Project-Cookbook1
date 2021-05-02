4825083e5d1d5e6dd33ba074c0ce10cedf78097d
ZitaBalint <zita.balint1072@gmail.com>
2021-05-02 22:02:40 +0000

    Added login functionality

--- app.py
+++ app.py
@@ -24,15 +24,10 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return render_template("login.html")
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        exicting_user = mongo.db.users.find_one(
+        existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
@@ -60,6 +55,29 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration was seccessful! Welcome to the community!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username".lower())
+                flash("Hi {}".format(request.form.get("username")))
+            else:
+                # invalid password
+                flash("Incorrect Username and/or Password, please try again!")
+                return redirect("login.html")
+        else:
+            # invalid username
+            flash("Incorrect Username and/or Password, please try again!")
+            return redirect("login.html")
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
