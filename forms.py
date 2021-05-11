141f1ba996c00c647fd7483fbb90bdc5a9342085
ZitaBalint <zita.balint1072@gmail.com>
2021-05-11 22:23:35 +0000

    Added UploadRecipeForm class to the file

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, PasswordField, SubmitField
+from flask_wtf.file import FileField
+from wtforms import StringField, BooleanField, PasswordField, SubmitField, TextAreaField, SelectMultipleField
 from wtforms import validators
 
 
@@ -12,7 +13,7 @@ class RegistrationForm(FlaskForm):
                               validators.Length(min=5, max=15)])
     confirm_password = PasswordField('Confirm Password',
                                      [validators.EqualTo('password',
-                                      message="Passwords should match")])
+                                                         message="Passwords should match")])
     submit = SubmitField('Register')
 
 
@@ -23,3 +24,24 @@ class LoginForm(FlaskForm):
                                           validators.Length(min=5, max=15)])
     remember_user = BooleanField('Stay Logged In')
     submit = SubmitField('Login')
+
+
+class UploadRecipeForm(FlaskForm):
+    image = FileField('Choose Image', [validators.DataRequired]),
+    uplaod_img = SubmitField('Upload File'),
+    recipe_title = StringField('Recipe Title', [
+                               validators.InputRequired,
+                               validators.Length(min=4, max=45)])
+    recipe_story = TextAreaField('Add a short story...', [
+                                 validators.DataRequired,
+                                 validators.Length(min=5, max=200)])
+    ingredients = StringField('Add ingredients by line', [
+                              validators.InputRequired])
+    steps = TextAreaField('Step by step method', [
+                          validators.DataRequired,
+                          validators.Length(min=5, max=260)])
+    categories = SelectMultipleField('Recipe category (PLease select at least one)',
+                                     choices=[('Breakfast'), ('Main meal'),
+                                              ('Snacks')])
+    keto_recipes = BooleanField('Is this a keto recipe?')
+    upload_recipe = SubmitField('Upload Recipe')
