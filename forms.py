5e708f40c2e6f0af8807b8dd084f788a298aa2e7
ZitaBalint <zita.balint1072@gmail.com>
2021-05-15 10:26:53 +0000

    added edit profile and edite recipe class

--- forms.py
+++ forms.py
@@ -46,3 +46,34 @@ class UploadRecipeForm(FlaskForm):
                                               ('Snacks', 'snacks')])
     keto_recipes = BooleanField('Is this a keto recipe?')
     upload_recipe = SubmitField('Upload Recipe')
+
+
+class EditProfileForm(FlaskForm):
+    profile_img = FileField('Profile Picture', [
+                            validators.InputRequired()])
+    user_bio = TextAreaField('Add Bio',
+                             [validators.InputRequired(),
+                              validators.Length(min=5, max=250)])
+    save_profile = SubmitField('Save')
+
+
+class EditRecipeForm(FlaskForm):
+    edit_image = FileField('Choose Image', [validators.DataRequired()]),
+    edit_recipeimg = SubmitField('Upload File'),
+    edit_recipe_title = StringField('Recipe Title', [
+        validators.InputRequired(),
+        validators.Length(min=4, max=45)])
+    edit_recipe_story = TextAreaField('Add a short story...', [
+        validators.DataRequired(),
+        validators.Length(min=5, max=200)])
+    edit_ingredients = StringField('Add ingredients by line', [
+        validators.InputRequired()])
+    edit_steps = TextAreaField('Step by step method', [
+        validators.DataRequired(),
+        validators.Length(min=5, max=260)])
+    edit_categories = SelectMultipleField('Recipe category (PLease select at least one)',
+                                          choices=[('Breakfast', 'breakfast'),
+                                                   ('Main meal', 'main'),
+                                                   ('Snacks', 'snacks')])
+    edit_keto_recipes = BooleanField('Is this a keto recipe?')
+    save_recipe = SubmitField('Save Changes')
